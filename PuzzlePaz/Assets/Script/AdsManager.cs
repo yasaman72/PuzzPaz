3956558f9112189018c7177a4febef7dd7a33b3e
Narges.F <nrgfar@gmail.com>
2018-12-10 17:45:14 +0330

    - Some changes in tapsell ad implementations

--- PuzzlePaz/Assets/Script/AdsManager.cs
+++ PuzzlePaz/Assets/Script/AdsManager.cs
@@ -10,4 +10,32 @@ public class AdsManager : MonoBehaviour
         string tapsellAppKey = "";
         Tapsell.initialize(tapsellAppKey);
     }
+
+    public void ShowTapsellVideoAd(string zoneID)
+    {
+        Tapsell.requestAd(zoneID, false,
+            (TapsellAd result) => //onAdAvailable
+            {
+                Debug.Log("Action: onAdAvailable");
+                TapsellAd ad = result; // store this to show the ad later
+            },
+            (string zoneId) => //onNoAdAvailable
+            {
+                Debug.Log("No Ad Available");
+            },
+            (TapsellError error) => //onError
+            {
+                Debug.Log(error.error);
+            },
+            (string zoneId) => //onNoNetwork
+            {
+                Debug.Log("No Network");
+            },
+            (TapsellAd result) => //onExpiring
+            {
+                Debug.Log("Expiring");
+                // this ad is expired, you must download a new ad for this zone
+            }
+        );
+    }
 }
